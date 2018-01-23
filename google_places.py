a6e8fa132ecfe380c668a387349331ef46713daf
Ben Everett <benjamin.r.everett@gmail.com>
2018-01-23 15:57:53 -0700

    updated code, need to add comments

--- google_places.py
+++ google_places.py
@@ -3,60 +3,57 @@
 # @Email:  benjamin.r.everett@gmail.com
 # @Filename: google_places.py
 # @Last modified by:   beneverett
-# @Last modified time: 01-22-2018
+# @Last modified time: 01-23-2018
 
 import requests
 import os
-import json
 
 
-class GooglePlaces(object):
+class HitGooglePlacesAPI(object):
 
     def __init__(self,
                  api_key=None,
                  internal_api_key=None):
 
         self.api_key = api_key
-        self._initialize(internal_api_key)
+        self.internal_api_key = internal_api_key
         self.requested_info = None
-
-    def _initialize(self, internal_api_key):
-        if self.api_key is None:
-            self.api_key = os.environ.get(internal_api_key)
 
     def fetch_metadata_info(self, address):
         self.meta_data_response = self._hit_api(api='meta_data',
                                                 address=address)
 
-    def fetch_radius_info(self, address, radius=5):
+    def fetch_radius_info(self, address, radius=100):
         lat_lng = self._get_lat_lng(address=address)
         self.radius_response = self._hit_api(api='radius',
-                                             location=lat_lng,
+                                             lat_lng=lat_lng,
                                              radius=radius)
 
     def fetch_detailed_info(self, address=None, place_id=None):
         if address:
-            response = self.fetch_radius_info(address=address)
             # take the number e.g. for '2801 ash st' we want '2801'
-            address.split(' ')[0]
+            num = address.split(' ')[0]
+            self.fetch_radius_info(address=address)
+            response = self.radius_response
+            place_ids = [response.json()['results'][i]['place_id']
+                         for i in range(len(response.json()['results']))
+                         if response.json()['results'][i]['vicinity']
+                         .split(' ')[0] == num]
+            self.detailed_response = [self._hit_api(api='details',
+                                                    place_id=i)
+                                      for i in place_ids]
+        else:
+            self.detailed_response = self._hit_api(api='details',
+                                                   place_id=place_id)
 
     def fetch_text_info(self, address):
         self.text_response = self._hit_api(api='text', address=address)
-
-        pass
-
-    def _get_lat_lng(self, address):
-        response = self._hit_api(api='meta_data',
-                                 address=address)
-        lat = response.json()['location']['lat']
-        lng = response.json()['location']['lng']
-
-        return lat, lng
+        return self.text_response
 
     def _hit_api(self,
                  api,
                  place_id=None,
-                 lat_lng=None,
+                 lat_lng=(0, 0),
                  radius=None,
                  address=None):
         link = self._api(api)
@@ -83,14 +80,14 @@ class GooglePlaces(object):
 
     def _payload_settings(self,
                           payload_name,
-                          place_id=None,
-                          lat_lng=None,
-                          radius=None,
-                          address=None):
+                          place_id,
+                          lat_lng,
+                          radius,
+                          address):
 
         api_key = self.api_key
-        if self.api_key is None:
-            api_key = os.environ.get(internal_api_key)
+        if api_key is None:
+            api_key = os.environ.get(self.internal_api_key)
 
         payloads = {
             "details":
@@ -110,39 +107,18 @@ class GooglePlaces(object):
 
         return payloads[payload_name]
 
-    def _get_details(self, address):
-        details_link = \
-            "https://maps.googleapis.com/maps/api/place/details/json?"
+    def _get_lat_lng(self, address):
+        response = self._hit_api(api='meta_data',
+                                 address=address)
+        lat = response.json()['location']['lat']
+        lng = response.json()['location']['lng']
 
-        num = address.split(' ')[0]
+        return lat, lng
 
-        self.businesses_ids = \
-            [self.info_radius.json()['results'][i]['place_id']
-             for i in range(len(self.info_radius.json()['results']))]
-             # if self.info_radius.json()
-             # ['results'][i]['vicinity'].split(' ')[0] == num]
-
-        self.info_detailed = []
-
-        for place_id in self.businesses_ids:
-            payload = {"place_id": place_id,
-                       "key": self.api_key}
-            self.info_detailed.append(requests.get(details_link, params=payload).json())
-
-        # self.place_id = self.info.json()['results']['vicinity'][counter]['place_id']
-
-    def _get_place_info_via_text(self, address):
-
-        text_link = \
-            "https://maps.googleapis.com/maps/api/place/textsearch/json?"
-
-        payload = {"query": address,
-                   "key": self.api_key}
-
-        self.info_text = requests.get(text_link, params=payload)
 
 if __name__ == '__main__':
-    place = GooglePlaces(internal_api_key="GOOGLE")
-    place.fetch_info(address='3000 lawrence street denver 80205')
-    print(len(place.info_detailed))
-    print(place.fetch_relevant_info())
+    place = HitGooglePlacesAPI(internal_api_key="GOOGLE")
+    place.fetch_metadata_info(address='5225 E 38th Ave, Denver, CO 80207')
+    place.fetch_radius_info(address='5225 E 38th Ave, Denver, CO 80207')
+    place.fetch_detailed_info(address='5225 E 38th Ave, Denver, CO 80207')
+    place.fetch_detailed_info(place_id='ChIJU5jhOsN7bIcR-X_Al6v1LSY')
